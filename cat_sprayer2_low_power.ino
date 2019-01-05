d680861d468647de3bf1782f3c8cf8241323728f
ma7801 <ma7801@gmail.com>
2019-01-04 20:58:24 -0800

    After major rewrite - no timers, only delays and interrupts

--- cat_sprayer2_low_power.ino
+++ cat_sprayer2_low_power.ino
@@ -1,8 +1,10 @@
 /* TODO 
  *  
  *  Code:
- *  - leave Arduino powered up when manually disabled (otherwise it will stay disabled forever!)
- *    - OR do some math with the sleep delay (i.e. SLEEP_8S) and run this in some kind of for loop to total to the amount of the desired disable duration
+ *  - Test thoroughly to see what needs to be done, namely:
+ *    - Test button during disabled state and inactivity state
+ *    - Test motion detecting during both of those states
+ *    - etc.
  *  
  *  Hardware:
  *  - need to move PIR input to D2
@@ -24,7 +26,9 @@
 
 #include <LowPower.h>
 
-enum intr {PIR = 1, BUTTON = 2};
+//enum intr {PIR = 1, BUTTON = 2};
+
+const int D = 500; // DEBUG delay for timer so that serial communication can happen before power down
 
 const int pirPin = 2;       
 const int sprayerPin = A5; 
@@ -33,11 +37,11 @@ const int LED2Pin = 5;
 const int buttonPin = 3;     // MUST be pin 2 or 3 since used as an external interrupt pin
 const int enabledLEDPin = 13;
 
-const period_t tempPowerDownDuration = SLEEP_8S;
+const period_t sleepIterationLength = SLEEP_8S;
 const int delayAfterEnable = 7;  // in seconds - pause value after going out of disabled state AND at power on (this is added on to power down delay above)
 const int delayAfterSpray = 7;  // in seconds
 const int sprayDuration = 250;   // in milliseconds
-const int disabledIncrement = 300; // in seconds  
+const int disabledIterations = 38; // number of 8 second "chunks" 
 const int powerDownDelay = 5;  // in seconds
 
 const int buttonDelay = 350; //in milliseconds
@@ -55,6 +59,9 @@ int buttonState;
 int LEDOnCount;
 int disabledDuration;
 
+// "Semaphore" flags (I think that's what they're called!):
+volatile bool interrupted_by_button;
+
 void setup() {
   Serial.begin(9600);
   
@@ -69,12 +76,13 @@ void setup() {
   LEDOnCount = 0;
   disabledDuration = 0;
   waitingForMotionTimerStart = millis();
+  interrupted_by_button = false;
   
   // This is to work out a bug that I can't quite figure out -- always sprays when initially enabled; we'll use this flag to ignore the "first spray trigger" and not actually spray
   firstSpray = true;
 
-  // Start off disabled (initializes enabled LED to false)
-  disableSprayer(delayAfterEnable); 
+  // Start off disabled (initializes enabled LED to false) for 2 iterations ~= 8 seconds
+  disableSprayer(1);   
 
   // Initialize pin states
   digitalWrite(pirPin, LOW);   //per HC-SR505 datasheet
@@ -90,7 +98,9 @@ void loop() {
   //See if we should save some power -- waiting too long 
   if(secondsSince(waitingForMotionTimerStart) > powerDownDelay) {
     // Set the arduino chip to a low power state (indefinitely - until interrupted by button or PIR input)
-    Serial.print("Going to sleep...\n");
+    Serial.println("Going to sleep due to inactivity...");
+    delay(D);
+    
     //Attach external interrupt to button pin
     attachInterrupt(digitalPinToInterrupt(buttonPin), wakeup_button, CHANGE);
     attachInterrupt(digitalPinToInterrupt(pirPin), wakeup_pir, CHANGE);
@@ -99,6 +109,9 @@ void loop() {
     // Should be execuated after wake up:
     detachInterrupt(digitalPinToInterrupt(buttonPin));
 
+    Serial.println("Waking up from inactivity.");
+    delay(D);
+    
     // Reset the waiting timer:
     waitingForMotionTimerStart = millis();
   }
@@ -113,6 +126,9 @@ void loop() {
     // Reset the button state
     buttonState = LOW;
 
+    // Delay so that a button press is not counted more than once in buttonDelay milliseconds
+    delay(buttonDelay);
+
     // Case 0: No LEDs currently on (sprayer enabled);  put in a disabled state and start disable timer
     if(LEDOnCount == 0) {
       
@@ -122,10 +138,10 @@ void loop() {
       // Turn on LED1
       digitalWrite(LED1Pin, HIGH);
 
-      // Disable sprayer for disabledIncrement seconds
-      disableSprayer(disabledIncrement);
+      // Disable sprayer for disabledIterations
+      disableSprayer(disabledIterations);
     }
-    
+    /*
     // Case 1: One LED on; set disabled state to double the disable increment (and restart timer)
     else if (LEDOnCount == 1) {
       // Set LED counter to 2 (i.e. two LEDs on)
@@ -135,7 +151,7 @@ void loop() {
       digitalWrite(LED2Pin, HIGH);
       
       // Disable sprayer for disabledIncrement*2 seconds
-      disableSprayer(disabledIncrement * 2);
+      disableSprayer(disabledIterations * 2);
 
     }
 
@@ -151,9 +167,8 @@ void loop() {
       disableSprayer(delayAfterEnable);
 
     }
-
-    // Delay so that a button press is not counted more than once in buttonDelay milliseconds
-    delay(buttonDelay);
+    */
+   
 
     // No need to run rest of code
     return;
@@ -162,7 +177,8 @@ void loop() {
   /* END Disable button code */
  
   /* Sprayer code */
-  
+
+  /*
   // If currently spraying:
   if(spraying) {
     // If spraying timer has elapsed, stop spraying!
@@ -185,8 +201,9 @@ void loop() {
     // Shouldn't need to run rest of code if spraying (or just finished spraying)
     return;
   }
+  */
   
-  
+  /*
   // If currently in a disabled state:
   if (disabled) {
 
@@ -211,6 +228,7 @@ void loop() {
     // Otherwise don't run rest of code
     else return;
   }
+  */
   
   // Read state of pin connected to motion sensor (PIR)
   pirInputValue = digitalRead(pirPin);
@@ -226,14 +244,14 @@ void loop() {
   if(pirInputValue == HIGH) {
     
     //DEBUG:
-    Serial.print("motion detected\n");
+    Serial.println("motion detected");
     
     // Reset pir state
     pirInputValue = LOW;
     
     // Start a timer for the sprayer, set spraying state to true, switch on sprayer
-    sprayTimerStart = millis();
-    spraying = true;
+    //sprayTimerStart = millis();
+    //spraying = true;
 
     // If first spray, don't actually spray!  This is a workaround for a bug I can't quite figure out...
     if(firstSpray) {
@@ -241,6 +259,14 @@ void loop() {
       return;
     }
     digitalWrite(sprayerPin, HIGH);
+    Serial.print("Pssssss...");
+    delay(sprayDuration);
+    digitalWrite(sprayerPin, LOW);
+    Serial.print("...tttt\n");
+    
+
+    //Disable sprayer for 2 iterations:
+    disableSprayer(2);
   }
 }
 
@@ -249,25 +275,72 @@ int secondsSince(unsigned long timeMS) {
 }
 
 
-void disableSprayer(int seconds) {
+void disableSprayer(int iterations) {
   // Set disabled state to true
-  disabled = true;
+  //disabled = true;
 
-
+  
   // Turn off enabled LED
   digitalWrite(enabledLEDPin, LOW);
-  disableTimerStart = millis();
-  disabledDuration = seconds;
+  //disableTimerStart = millis();
+  //disabledDuration = seconds;
+  
+  // Set the arduino chip to a low power state
+  Serial.print("Going to sleep due to disable...\n");
 
-  // Set the arduino chip to a low power state (temporarily - 8 seconds)
-  Serial.print("Going to sleep...\n");
+  //DEBUG
+  delay(D);
+  
   //Attach external interrupt to button pin
   attachInterrupt(digitalPinToInterrupt(buttonPin), wakeup_button, CHANGE);
   // Power down
-  LowPower.powerDown(tempPowerDownDuration, ADC_OFF, BOD_OFF);
+  for (int i = 0; i < iterations; i++) {
+    LowPower.powerDown(sleepIterationLength, ADC_OFF, BOD_OFF);
+    
+    // If an external interrupt occurs, break out of loop (don't keep powering down!)
+    if(interrupted_by_button) {
+      interrupted_by_button = false;
+
+      // Case 1: Only LED1 is lit; restart disabled mode with double the iteration length:
+      if (LEDOnCount == 1) {
+        iterations = disabledIterations * 2;
+        
+        // Turn on LED2
+        digitalWrite(LED2Pin, HIGH);
+        
+        // Now 2 LEDs are on
+        LEDOnCount = 2;
+
+        // Delay by button delay amount (so that a single button input doesn't get counted again!)
+        delay(buttonDelay);
+      }
+      else if (LEDOnCount == 2) {
+        //Turn off both LEDs
+        turnOffLEDs();
+        
+        LEDOnCount = 0;
+
+        // Delay by button delay amount (so that a single button input doesn't get counted again!)
+        delay(buttonDelay);
+
+        // Break out of the loop so that arduino doesn't continue sleeping!
+        break;
+      }
+      else {
+        // Shouldn't happen
+        Serial.print("Error in button handler");
+      }
+      
+    }
+  }
+
   // Should be execuated after wake up:
   detachInterrupt(digitalPinToInterrupt(buttonPin));
 
+  //DEBUG
+  Serial.println("Waking up from disable naturally...");
+  delay(D);
+  
   // Set pir PIN to low
   //digitalWrite(pirPin, LOW);
 
@@ -283,9 +356,11 @@ void turnOffLEDs() {
 void wakeup_pir() {
 
   Serial.println("Waking up...PIR input!");
+  //interrupted_by_pir = true;
 
 }
 
 void wakeup_button() {
   Serial.println("Waking up...button input!");
+  interrupted_by_button = true;
 }
